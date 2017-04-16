62f0ab53dadd05e2f499f7fb502cda51d1554bc0
Jumpei Matsuda <j.matsuda.bb26th@gmail.com>
2017-04-16 13:29:26 +0900

    Updated comments

--- SampleApplication/app/src/main/java/com/fatdaruma/sampleapplication/MainActivity.java
+++ SampleApplication/app/src/main/java/com/fatdaruma/sampleapplication/MainActivity.java
@@ -12,8 +12,16 @@ public class MainActivity extends AppCompatActivity {
 
         InternalSample sample = new InternalSample();
 
+        // Java can access
         sample.publicMethod();
+
+        // Java can access
         sample.protectedMethod();
+
+        // Java can see a method below from AndroidStudio, but it causes an compile error due to missing method
         sample.internalMethod$production_sources_for_module_app();
+
+        // Java cannot see a method below from both of AndroidStudio and javac
+        sample.internalMethod();
     }
 }
